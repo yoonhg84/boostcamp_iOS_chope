734be756846417708439cdda00ecb21415917d98
jjy4880 <jjy48801@naver.com>
2017-07-09 22:29:57 +0900

    5장과제

---
+++ week2/WorldTrotter_5/WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by User on 2017. 7. 7..
+//  Copyright © 2017년 Ji-Yong Jeong. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+    
+    
+}
